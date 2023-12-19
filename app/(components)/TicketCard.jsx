4e5759d29ee7c8ca24945be9cce1b1052a3fde6c
Prashasti Karlekar <prashastikarlekar@gmail.com>
2023-12-18 23:15:20 -0600

    Read Tickets completed.

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -5,28 +5,42 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
+	const formatTimestamp = (timestamp) => {
+		const options = {
+			year: "numeric",
+			month: "2-digit",
+			day: "2-digit",
+			hour: "2-digit",
+			minute: "2-digit",
+			hour12: true,
+		};
+		const date = new Date(timestamp);
+		const formattedDate = date.toLocaleString("en-Us", options);
+		return formattedDate;
+	};
+
 	return (
 		<div className='flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2'>
 			<div className='flex mb-3 '>
-				<PriorityDisplay />
+				<PriorityDisplay priority={ticket.priority} />
 				<div className='ml-auto'>
 					<DeleteBlock />
 				</div>
 			</div>
-			<h4>Ticket Title</h4>
+			<h4>{ticket.title}</h4>
 			<hr className='h-px border-0 bg-page mb-2' />
-			<p className='whitespace-pre-wrap'>this is the ticket description.</p>
+			<p className='whitespace-pre-wrap break-all'>{ticket.description}</p>
 			{/* below line will ensure that ticket cards are of same size regardless of their content length*/}
 			<div className='flex-grow'></div>
 			<div className='flex mt-2'>
 				<div className='flex flex-col'>
-					<p className='text-xs my-1'>12/17/2023 6:06PM</p>
+					<p className='text-xs my-1'>{formatTimestamp(ticket.createdAt)}</p>
 
-					<ProgressDisplay />
+					<ProgressDisplay progress={ticket.progress} />
 				</div>
 				<div className='ml-auto flex-items-end'>
-					<StatusDisplay />
+					<StatusDisplay status={ticket.status} />
 				</div>
 			</div>
 		</div>
